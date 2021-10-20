140246b00acfb77ad139f4e391d35c8f6fe0f787
Linus Gellhorn <linus.g@hotmail.com>
2021-10-20 18:44:08 +0100

    Exclamation test

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,7 +1,7 @@
 function PageHeader(): JSX.Element {
   return (
     <header>
-      <h1>Nichard's Journal</h1>
+      <h1>Nichard's Journal!!!!!</h1>
       <p>This is my public journal.</p>
     </header>
   );
